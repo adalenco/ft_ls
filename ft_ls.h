30b3cceb6a7c23656d93cb69751fa15202f464a2
adalenco <adalenco@student.42.fr>
2017-09-15 14:53:14 +0200

    bdfjkd

--- ft_ls.h
+++ ft_ls.h
@@ -6,17 +6,26 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
-#define OP_L (options[0])
-#define OP_RR (options[1])
-#define OP_A (options[2])
-#define OP_R (options[3])
-#define OP_T (options[4])
+# define OP_L (options[0])
+# define OP_RR (options[1])
+# define OP_A (options[2])
+# define OP_R (options[3])
+# define OP_T (options[4])
+
+typedef struct s_struct
+{
+	
+}				t_struct;
+
+typedef	struct		s_list
+{
+	char			mode[11];
+	char			*name;
+	unsigned int	size;
+	void			*next;
+}					t_list;
 
 char		*parse_av(char *options, char **av);
 void		ft_putstr(char *str);
-typedef struct s_struct
-{
-
-}				t_struct;
 
 #endif
--- main.c
+++ main.c
@@ -25,19 +25,23 @@ void		ft_print_ls(DIR *dirp, char options[6])
 	struct dirent *entry;
 	struct stat buf;
 	int			dir;
+	t_list		*list;
 	
 	dir = 0;
+
 	printf("%-15s      name\n%-15s%s\n\n", "size", "----", "      ----");
-	while ((entry = readdir(dirp))  != NULL)
+	while ((entry = readdir(dirp))  != NULL && (list = (t_list *)malloc(sizeof(t_list))) != NULL)
 	{
+		b_zero(list->mode, 11);
 		if (entry->d_name[0] == '.' && OP_A == 0)
 			;
 		else
 		{
 			stat(entry->d_name, &buf);
-			printf("%-15u", (unsigned int)buf.st_size);
+			list->size = (unsigned int)buf.st_size;
 			if (S_ISDIR(buf.st_mode))
 			{
+				
 				dir++;
 				printf("d     ");
 			}
@@ -56,11 +60,10 @@ int			main(int ac, char **av)
 	int		i;
 	char	options[6];
 	char		*path;
-	char		mode[11];
 
 	path = "./";
 	b_zero(options, 6);
-	b_zero(mode, 11);
+
 	i = 0;
 	if (ac > 1)
 		parse_av(options, av);
